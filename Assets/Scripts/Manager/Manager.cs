1dbcc43fc19187dc5ceb4a6aa13f289b65d33a3b
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-22 21:16:10 +0900

    [Feat] UIManager 스크립트 추가.
    전역적인 접근이 필요한 UI 요소를 가지고 있을 UIManager 스크립트 추가.

--- Assets/Scripts/Manager/Manager.cs
+++ Assets/Scripts/Manager/Manager.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 public static class Manager
 {
     public static PlayerManager Player => PlayerManager.GetInstance();
+    public static UIManager UI => UIManager.GetInstance();
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Initailize()
     {
         PlayerManager.CreateInstance();
+        UIManager.CreateInstance();
     }
 }
