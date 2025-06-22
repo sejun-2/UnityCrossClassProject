39fef5e53fe2da7bd7b4b3205c23d1e087a6610f
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-22 22:16:29 +0900

    [Feat] PlayerManager에 Transform 변수 추가
    현재 씬에 생성된 Player 오브젝트를 추적하기 위해 Transform 변수 추가.
    
    이후 Player 딴 스크립트 Awake 등에서 이 변수에 Transform을 등록할 예정.

--- Assets/Scripts/Manager/PlayerManager.cs
+++ Assets/Scripts/Manager/PlayerManager.cs
@@ -6,4 +6,7 @@ public class PlayerManager : Singleton<PlayerManager>
 {
     // TODO : 스탯 초기화 로직 작성
     public PlayerStats Stat = new();
+
+    // 현재 생성된 Player 오브젝트의 Transform
+    public Transform Transform;
 }
