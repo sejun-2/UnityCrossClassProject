8c73b2706991e63c4ffd3134df734900b6755f54
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-23 16:54:57 +0900

    [Feat] Inventory 스크립트 추가
    Player의 인벤토리에 대한 데이터를 가지고 있는 Inventory 스크립트 추가.
    
    인벤토리는 슬롯 2중 배열을 가지고 관리한다.

---
+++ Assets/WorkSpace/JTW/Scripts/Inventory.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Inventory
+{
+    private Slot[,] _slotList = new Slot[4,4];
+    public Slot[,] SlotList => _slotList;
+
+    public Inventory()
+    {
+        for(int i = 0; i < _slotList.GetLength(0); i++)
+        {
+            for(int j = 0; j < _slotList.GetLength(1); j++)
+            {
+                _slotList[i, j] = new Slot();
+            }
+        }
+    }
+
+    public Inventory(Item item)
+    {
+        for (int i = 0; i < _slotList.GetLength(0); i++)
+        {
+            for (int j = 0; j < _slotList.GetLength(1); j++)
+            {
+                _slotList[i, j] = new Slot(item);
+            }
+        }
+    }
+}
