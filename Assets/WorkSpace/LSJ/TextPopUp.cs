9079b03b6ac19469890eba0e1a7e1e8a97c4b65d
sejun <sejun2.lee@gmail.com>
2025-06-25 15:58:42 +0900

    [Fix] messages 내용이 초기화 되지 않는 버그 수정

--- Assets/WorkSpace/LSJ/TextPopUp.cs
+++ Assets/WorkSpace/LSJ/TextPopUp.cs
@@ -10,22 +10,24 @@ public class TextPopUp : BaseUI
 {
     public TextMeshProUGUI uiText;         // 출력할 UI Text
     //public string message;      // 출력할 대사
-    public string[] messages = {
-        "낮에는 포격과 저격수들의 위험으로 때문에 돌아 다닐수 없고",
-        "밤에는 강도들이 피난처를 노리는 절망적인 상황입니다.",
-        "우리는 살아남기 위해 최선을 다해야 합니다."
-    };
+    public string[] messages; // 여러 줄의 메시지를 저장할 배열
 
     public float typingSpeed = 0.05f; // 글자 출력 간격(초)
     public float lineDelay = 1.0f; // 한 줄 끝나고 다음 줄까지 대기 시간
 
-    //void Awake()
-    //{
+    void Awake()
+    {
 
-    //    message = "안녕하세요! 이 메시지는 타이핑 효과로 출력됩니다. " +
-    //        "이것은 Unity에서 코루틴을 사용하여 글자를 하나씩 출력하는 예제입니다."; // 메시지를 바로 출력합니다.
+        //message = "안녕하세요! 이 메시지는 타이핑 효과로 출력됩니다. " +
+        //            "이것은 Unity에서 코루틴을 사용하여 글자를 하나씩 출력하는 예제입니다.";
 
-    //}
+        messages = new string[] {
+        "낮에는 포격과 저격수들의 위험으로 때문에 돌아 다닐수 없고",
+        "밤에는 강도들이 피난처를 노리는 절망적인 상황입니다.",
+        "우리는 살아남기 위해 최선을 다해야 합니다."
+        };
+
+    }
 
     void Start()
     {
