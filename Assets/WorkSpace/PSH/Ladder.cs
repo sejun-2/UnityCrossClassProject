a5c1148de8ebc1c17873f0eb638b374e29d35a86
xmasoo <xmasoo73@gmail.com>
2025-06-25 10:35:17 +0900

    [Feat] Ladder Script 수정

--- Assets/WorkSpace/PSH/Ladder.cs
+++ Assets/WorkSpace/PSH/Ladder.cs
@@ -2,7 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Ladder : MonoBehaviour
+public class Ladder : MonoBehaviour, IInteractable
 {
-   
+    public Transform climbStartPoint;  // 사다리 타기 시작 위치
+    public Transform climbEndPoint;    // 사다리 끝
+    public Test player;
+    public void Interact()
+    {
+        player.Climb();
+    }
 }
