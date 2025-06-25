90b6e9cd9533c8a84fb4a15fc7757118bfdff67c
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-25 14:49:22 +0900

    [Feat] FarmingObject 스크립트 추가.
    Player가 상호작용 시 FarmingObject를  띄우는 기능의 FarmingObject 스크립트 추가.

---
+++ Assets/WorkSpace/JTW/Scripts/Farming/FarmingObject.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FarmingObject : MonoBehaviour, IInteractable
+{
+    // 테스트용 아이템
+    [SerializeField] private Item _testItem;
+
+    private Inventory _farmingInven;
+
+    private void Awake()
+    {
+        _farmingInven = new Inventory();
+
+        // 테스트용 아이템 추가.
+        _farmingInven.AddItem(_testItem);
+    }
+
+    public void Interact()
+    {
+        Manager.UI.Inven.ShowTradeFarming(_farmingInven);
+    }
+}
