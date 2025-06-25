647c321d58b1be56053e9847fc9003d243f5ec24
xmasoo <xmasoo73@gmail.com>
2025-06-25 17:19:09 +0900

    [Feat] Outline 기능 구현
    오브젝트 앞에 트리거영역을 지나가면 아웃라인이 활성화되는 기능

--- Assets/WorkSpace/PSH/OutLineVisible.cs
+++ Assets/WorkSpace/PSH/OutLineVisible.cs
@@ -1,8 +1,35 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class OutLineVisible : MonoBehaviour
 {
-    
+    private Renderer _renderer;
+    private Material _mat;
+
+    [SerializeField] private float onScale = 1.2f;
+    [SerializeField] private float offScale = 0f;
+
+    private void Awake()
+    {
+        _renderer = GetComponent<Renderer>();
+        if (_renderer != null && _renderer.materials.Length > 1)
+        {
+            _mat = _renderer.materials[1]; // 두 번째 머테리얼이 아웃라인
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && _mat != null)
+        {
+            _mat.SetFloat("_Scale", onScale);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player") && _mat != null)
+        {
+            _mat.SetFloat("_Scale", offScale);
+        }
+    }
 }
