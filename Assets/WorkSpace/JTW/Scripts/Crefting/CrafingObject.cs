72b0a32ec25fb5c0e545daeee91a61c1396142c1
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-26 16:21:36 +0900

    [Feat] CraftingObject 스크립트 추가
    상호작용 시 제작창을 띄우는 CraftingObject 추가.

---
+++ Assets/WorkSpace/JTW/Scripts/Crefting/CrafingObject.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrafingObject : MonoBehaviour, IInteractable
+{
+    public void Interact()
+    {
+        Manager.UI.Inven.ShowCraftingUI();
+    }
+}
