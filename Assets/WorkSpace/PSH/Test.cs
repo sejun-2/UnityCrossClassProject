c76847c8518fb282eb9fc9d0a96464fc672073de
xmasoo <xmasoo73@gmail.com>
2025-06-27 17:31:22 +0900

    [Test] 테스트 스크립트
    좀비한테 데미지주는 테스트 스크립트

--- Assets/WorkSpace/PSH/Test.cs
+++ Assets/WorkSpace/PSH/Test.cs
@@ -2,5 +2,12 @@ using UnityEngine;
 
 public class Test : MonoBehaviour
 {
-    
+   [SerializeField] Zombie zombie;
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            zombie.TakeDamage(1000f);
+        }
+    }
 }
