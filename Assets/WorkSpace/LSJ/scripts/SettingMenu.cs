48ac2b8173ff43a6ff7cbe8aea12f817aeb69d13
sejun <sejun2.lee@gmail.com>
2025-06-27 12:14:06 +0900

    [feat] 키보드로 선택 및 하이라이트 추가

--- Assets/WorkSpace/LSJ/scripts/SettingMenu.cs
+++ Assets/WorkSpace/LSJ/scripts/SettingMenu.cs
@@ -6,9 +6,18 @@ public class SettingMenu : BaseUI
 {
     private void Start()
     {
-        Debug.Log(GetEvent("SelectButton")); // SettingMenu UI가 시작될 때 로그를 출력합니다.
+        Debug.Log(GetEvent("SettingMenu열림")); // SettingMenu UI가 시작될 때 로그를 출력합니다.
         // SelectButton 버튼이 클릭되었을 때 호출되는 메서드
         GetEvent("SelectButton").Click += data => Manager.UI.PopUp.ShowPopUp<LangguagePopUp>(); // LangguagePopUp을 표시합니다.
-        GetEvent("SelectText").Click += data => Manager.UI.PopUp.ShowPopUp<LangguagePopUp>();
+        GetEvent("SelectText").Enter += data => Manager.UI.PopUp.ShowPopUp<LangguagePopUp>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Manager.UI.PopUp.ShowPopUp<LangguagePopUp>();
+        }
     }
 }
+
