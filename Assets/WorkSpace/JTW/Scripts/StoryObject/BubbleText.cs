30872b466a4692b41004e77244071eb7146cd3f7
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-28 21:48:49 +0900

    [Feat] BubbleText 스크립트 추가
    말풍선을 띄울 시 글자에 따라 크기를 조정하는 기능을 가진 BubbleText 스크립트 추가.

---
+++ Assets/WorkSpace/JTW/Scripts/StoryObject/BubbleText.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class BubbleText : MonoBehaviour
+{
+    private TextMeshProUGUI _text;
+
+    private void Awake()
+    {
+        _text = GetComponentInChildren<TextMeshProUGUI>();
+    }
+
+    public void SetText(string content)
+    {
+        _text.text = content;
+        RectTransform rt = _text.GetComponent<RectTransform>();
+        rt.sizeDelta = new Vector2(Mathf.Min(500, _text.preferredWidth), 30);
+        rt.sizeDelta = new Vector2(rt.sizeDelta.x, _text.preferredHeight);
+    }
+}
