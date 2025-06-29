76cae4d0dcb9f2c3a7df594cebea3ab0e8c06e5a
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-29 17:00:27 +0900

    [Feat] MapPresenter 스크립트 추가
    Map 선택 UI를 관리할 MapPresenter 스크립트 추가.
    
    각 맵 슬롯을 이동하며 맵에 대한 정보를 확인할 수 있고, 선택하면 아이템 창고에서 아이템을 챙기는 화면으로 이동한다.

---
+++ Assets/WorkSpace/JTW/Scripts/Map/MapUIPresenter.cs
@@ -0,0 +1,80 @@
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MapUIPresenter : BaseUI
+{
+    [SerializeField] private GameObject _itemSlotUIsPrefab;
+
+    private GameObject _mapSlotsPanel;
+    private ItemSlotUIs _mapSlotUIs;
+    private List<MapPoint> _mapPoints;
+
+    private int _selectedMapIndex;
+
+    private void Start()
+    {
+        InitMapUI();
+    }
+
+    private void Update()
+    {
+        MoveInventory();
+
+        if (Input.GetKeyDown(KeyCode.Z))
+        {
+            Manager.Game.SelectedMapName = _mapPoints[_selectedMapIndex].SceneName;
+            Manager.UI.Inven.ShowTradeItemBox();
+            Destroy(gameObject);
+        }
+    }
+
+    private void InitMapUI()
+    {
+        _mapSlotsPanel = GetUI("MapSlotsPanel");
+
+        _mapSlotUIs = Instantiate(_itemSlotUIsPrefab, _mapSlotsPanel.transform).GetComponent<ItemSlotUIs>();
+        _mapSlotUIs.SetPanelSize(new Vector2(1, 5));
+        _mapSlotUIs.SetLineCount(1);
+
+        _mapPoints = GetComponentsInChildren<MapPoint>().ToList();
+
+        for(int i = 0; i < _mapPoints.Count; i++)
+        {
+            _mapPoints[i].InitData();
+            _mapSlotUIs.AddSlotUI();
+            _mapSlotUIs.SlotUIs[i].SetText(_mapPoints[i].SceneName);
+        }
+
+        _mapSlotUIs.SelectSlotUI(0);
+        UpdateMapDescription();
+    }
+
+    private void MoveInventory()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            ChangeSelectSlot(Vector2.up);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            ChangeSelectSlot(Vector2.down);
+        }
+    }
+
+    private void ChangeSelectSlot(Vector2 movePos)
+    {
+        _mapSlotUIs.MoveSelectSlot(movePos);
+        UpdateMapDescription();
+    }
+
+    private void UpdateMapDescription()
+    {
+        _mapPoints[_selectedMapIndex].SetSelect(false);
+
+        _selectedMapIndex = _mapSlotUIs.SelectedSlotIndex;
+
+        _mapPoints[_selectedMapIndex].SetSelect(true);
+    }
+}
