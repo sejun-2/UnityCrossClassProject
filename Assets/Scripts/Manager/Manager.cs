821c738a6267530030e0a07df5d84eb48bca27ce
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-29 22:02:42 +0900

    [Feat] Manager에 Sound 추가
    Manager에서 Sound에 접근할 수 있도록 코드 및 프리팹 생성.

--- Assets/Scripts/Manager/Manager.cs
+++ Assets/Scripts/Manager/Manager.cs
@@ -8,6 +8,7 @@ public static class Manager
     public static UIManager UI => UIManager.GetInstance();
     public static GameManager Game => GameManager.GetInstance();
     public static DataManager Data => DataManager.GetInstance();
+    public static SoundManager Sound => SoundManager.GetInstance();
 
     [RuntimeInitializeOnLoadMethod(RuntimeInitializeLoadType.BeforeSceneLoad)]
     private static void Initailize()
@@ -16,5 +17,6 @@ public static class Manager
         UIManager.CreateInstance();
         GameManager.CreateInstance();
         DataManager.CreateInstance();
+        SoundManager.CreateInstance();
     }
 }
