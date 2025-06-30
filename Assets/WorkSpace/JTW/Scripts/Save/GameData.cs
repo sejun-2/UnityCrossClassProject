095087b6b6bfee40d5055c22ccb1a5990a21ed05
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-06-30 12:09:27 +0900

    [Feat] GameData에 아이템 내구도 추가
    현재 내구도도 저장될 수 있도록 내구도 변수 추가.

--- Assets/WorkSpace/JTW/Scripts/Save/GameData.cs
+++ Assets/WorkSpace/JTW/Scripts/Save/GameData.cs
@@ -1,15 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+public struct ItemSaveData
+{
+    public string Id;
+    public int Durability;
+}
 
 public class GameData
 {
-    public PlayerStats stats;
-    public string WeaponId;
-    public string ArmorId;
+    
 
-    public Dictionary<string, int> InvenData = new Dictionary<string, int>();
-    public Dictionary<string, int> ItemBoxData = new Dictionary<string, int>();
+    public PlayerStats stats;
+    public ItemSaveData WeaponData;
+    public ItemSaveData ArmorData;
+
+    public Dictionary<ItemSaveData, int> InvenData = new Dictionary<ItemSaveData, int>();
+    public Dictionary<ItemSaveData, int> ItemBoxData = new Dictionary<ItemSaveData, int>();
 
     public Dictionary<string, bool> IsRepairObject = new Dictionary<string, bool>();
     public Dictionary<string, bool> IsUsedObject = new Dictionary<string, bool>();
