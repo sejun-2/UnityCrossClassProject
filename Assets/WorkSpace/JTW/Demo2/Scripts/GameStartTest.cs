238fb09032aa178037b7c45b873914508282e45e
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-01 13:59:54 +0900

    [Feat] GameStartTest 스크립트 추가
    GameManager에 GameStart를 실험해볼 수 있도록 스크립트 추가

---
+++ Assets/WorkSpace/JTW/Demo2/Scripts/GameStartTest.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameStartTest : MonoBehaviour
+{
+    public void GameStart()
+    {
+        Manager.Game.GameStart();
+    }
+}
