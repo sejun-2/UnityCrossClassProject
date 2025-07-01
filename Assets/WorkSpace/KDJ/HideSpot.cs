e65141abb92548b76a4a7553a4234fc0d8270a0a
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-01 14:52:08 +0900

    [Fix] HideSpot 임시로 정의
    hidePosition이 없어서 컴파일 오류 나니까 임시로 생성.

--- Assets/WorkSpace/KDJ/HideSpot.cs
+++ Assets/WorkSpace/KDJ/HideSpot.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class HideSpot : MonoBehaviour
 {
+    public Transform hidePosition;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        hidePosition = transform;
     }
 
     // Update is called once per frame
