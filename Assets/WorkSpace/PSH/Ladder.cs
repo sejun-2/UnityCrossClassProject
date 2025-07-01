73e9d17835537edd0dfd5ea28bed6d6cee3df9d9
xmasoo <xmasoo73@gmail.com>
2025-07-01 10:57:59 +0900

    [Fix] 사다리 상호작용
    사다리 위층에서 위로 상호작용 되는 문제 해결

--- Assets/WorkSpace/PSH/Ladder.cs
+++ Assets/WorkSpace/PSH/Ladder.cs
@@ -21,6 +21,25 @@ public class Ladder : MonoBehaviour, IInteractable
     }
     public void Interact(Transform player, bool goUp, float climbSpeed, PlayerInteraction interaction)
     {
+        float distToTop = Vector3.Distance(player.position, topPoint.position);
+        float distToBottom = Vector3.Distance(player.position, bottomPoint.position);
+
+        if (goUp)
+        {
+            if (distToTop < distToBottom)
+            {
+                Debug.Log("이미 사다리 위에 있음. 위로 이동 불가.");
+                return;
+            }
+        }
+        else
+        {
+            if (distToBottom < distToTop)
+            {
+                Debug.Log("이미 사다리 아래에 있음. 아래로 이동 불가.");
+                return;
+            }
+        }
         Vector3 from = goUp ? bottomPoint.position : topPoint.position;
         Vector3 to = goUp ? topPoint.position : bottomPoint.position;
 
