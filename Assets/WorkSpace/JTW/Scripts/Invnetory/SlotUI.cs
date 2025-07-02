0c6fbdd1ffaefeb2b026a7cafc25073a5804ea3b
JitaeWoo <94220257+JitaeWoo@users.noreply.github.com>
2025-07-02 12:01:17 +0900

    [Fix] Bulid 불가 using문 삭제
    Build시 문제가 되는 using문 삭제

--- Assets/WorkSpace/JTW/Scripts/Invnetory/SlotUI.cs
+++ Assets/WorkSpace/JTW/Scripts/Invnetory/SlotUI.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
-using static UnityEditor.Progress;
-using static UnityEngine.Rendering.DebugUI;
 
 public class SlotUI : MonoBehaviour
 {
